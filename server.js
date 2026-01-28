820da08830899cd84598b56086d293854f9d13cd
shubhamk-par <shubham.kania@parsolution.net>
2026-01-28 12:55:23 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const server = require("http").createServer(app);
+app.get("/", (req, res) => {
+  res.send("Hello World! From GitHub CI/CD Pipeline");
+});
+
+server.listen(5050, () => {
+  console.log("Server is running on port 5050");
+});
