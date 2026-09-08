86771ef7c1de30eaec8c3ad95b6940ba6125d40e
shubhamk-par <shubham.kania@10turtle.com>
2026-09-08 18:04:38 +0530

    fix the code

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import http from "http";
+
 const app = express();
 const server = http.createServer(app);
+
+const PORT = process.env.PORT || 5050;
+
 app.get("/", (req, res) => {
   res.send("Hello World! From GitHub CI/CD Pipeline ! hello 10turtle ...");
 });
 
-server.listen(8988, () => {
-  console.log("Server is running on port 5050");
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
