2b4faec17bcbe3d1564d861ddb32df7883475b33
shubhamk-par <shubham.kania@10turtle.com>
2026-09-10 11:07:54 +0530

    update the comment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 5050;
 
 app.get("/", (req, res) => {
-  res.send("Hello World ! From GitHub CI/CD Pipeline ! hello Wibes ...");
+  res.send("Hello World ! From GitHub CI/CD Pipeline ! hello Friendssssss ...");
 });
 
 server.listen(PORT, "0.0.0.0", () => {
